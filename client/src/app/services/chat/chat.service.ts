993a87bf49be3b8087be354b6f81cbbbf9649536
Julien9969 <julien.roux404@gmail.com>
2023-04-03 17:56:10 -0400

    meileur score chat ok -> test + auto scroll a faire

--- client/src/app/services/chat/chat.service.ts
+++ client/src/app/services/chat/chat.service.ts
@@ -9,8 +9,8 @@ import { SystemMessage } from '@common/systemMessage';
     providedIn: 'root',
 })
 export class ChatService {
-    chatContainer: ElementRef<HTMLDivElement>;
     messageList: Message[];
+    formElement: ElementRef<HTMLFormElement>;
     clientId: string;
 
     constructor(public socketService: SocketClientService) {
@@ -61,11 +61,10 @@ export class ChatService {
         this.scrollToBottom();
     }
     scrollToBottom() {
-        this.chatContainer.nativeElement.scrollTop = this.chatContainer.nativeElement.scrollHeight;
+        this.formElement.nativeElement.scrollIntoView();
     }
     async listenForSystemMessage() {
         this.socketService.on(ChatEvents.SystemMessageFromServer, (systemMessage: SystemMessage) => {
-            console.log('newMessage');
             this.receiveMessage(this.createSystemMessage(systemMessage.systemCode, systemMessage.playerName));
         });
     }
@@ -93,4 +92,5 @@ export class ChatService {
             });
         });
     }
+
 }
