1ca3ff1623e2e3bf82d5595ebcbdc8f3ccbd0089
Sebastien Roy <sebastien-6.roy@polymtl.ca>
2023-04-20 19:22:40 +0000

    Feature/18 indices de jeu 1.1 - remise finale

--- client/src/app/services/constantes.service.ts
+++ client/src/app/services/constantes.service.ts
@@ -11,3 +11,9 @@ export enum SystemCode {
 
 export const RATIO_POINTER_IMAGE = 0.4;
 export const POINTER_X_OFFSET = 15;
+
+/**
+ * constantes game-action-logging service
+ */
+export const INIT_BASE_TIME_INCREMENT = 47;
+export const LAST_INDEX = -1;
