2afcc8443a1e22bd250793a23e21ea3fe1dd5e01
Julien9969 <julien.roux404@gmail.com>
2023-04-20 18:18:59 -0400

    suppression commentaire

--- client/src/app/components/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -19,7 +19,6 @@ export class SidebarComponent implements AfterViewInit, AfterViewChecked {
     });
 
     constructor(private formBuilder: FormBuilder, public chatService: ChatService, public imageOperationService: ImageOperationService) {
-        // this.chatService.formElement = this.formElement; // De branche de max, necessaire?
         this.chatService.start();
     }
 
